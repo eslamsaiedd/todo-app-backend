5f03d87f44494c3035d9f2d2531884c0a65d2ed9
eslam <eslamsaied051@gmail.com>
2025-10-25 19:37:03 +0300

    fix path error

--- index.js
+++ index.js
@@ -2,20 +2,19 @@
 require('dotenv').config();
 const express  = require('express')
 const app = express()
-const path = require('path');
 const cors = require('cors');
 
 
-// const allowedOrigins = [
-//   'https://classy-shortbread-ec361f.netlify.app', // موقعك على Netlify
-//   'http://localhost:4000' // أثناء التطوير المحلي
-// ];
+const allowedOrigins = [
+  'https://classy-shortbread-ec361f.netlify.app', // موقعك على Netlify
+  'http://localhost:4000' // أثناء التطوير المحلي
+];
 
-// app.use(cors({
-//   origin: allowedOrigins,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 
@@ -34,13 +33,6 @@ app.use('/api/users', userRoutes);
 app.use("/api/tasks", taskRoutes);
 
 
-
-app.use(express.static(path.join(__dirname, 'front-end')));
-
-// لو فتح أي URL مش موجود، يرجع index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front-end', 'index.html'));
-});
 
 
 app.use((err, req, res, next) => {
