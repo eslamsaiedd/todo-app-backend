dbea775c0212d418d47ecaf7b54d262fc7fbdbef
eslam <eslamsaied051@gmail.com>
2025-10-25 19:32:40 +0300

    fix path error

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const path = require('path');
 const cors = require('cors');
 
 
-const allowedOrigins = [
-  'https://classy-shortbread-ec361f.netlify.app', // موقعك على Netlify
-  'http://localhost:4000' // أثناء التطوير المحلي
-];
+// const allowedOrigins = [
+//   'https://classy-shortbread-ec361f.netlify.app', // موقعك على Netlify
+//   'http://localhost:4000' // أثناء التطوير المحلي
+// ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: allowedOrigins,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }));
 
 app.use(express.json());
 
